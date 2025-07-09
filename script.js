f9162880cc755148554df09ed10df290dac0b667
badrudoza <106438583+badrudoza@users.noreply.github.com>
2025-07-10 00:02:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function functionName() {
+console.log(arguments.callee.name);
+}
+function accioJob() {
+	console.log(arguments.callee.name);
+}
+functionName();
+accioJob();
